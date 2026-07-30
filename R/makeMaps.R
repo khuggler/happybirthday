5f8f4f299cfa8c432090d3b7f53e7adc5418b072
Huggler <katey.huggler@umt.edu>
2026-07-30 09:19:55 -0600

    make maps

--- R/makeMaps.R
+++ R/makeMaps.R
@@ -186,9 +186,9 @@ for(n in seq_along(uni)) {
     cat("\nITERATION:", n, "\n")
     cat("class(a):", paste(class(a), collapse = ", "), "\n")
     cat("typeof(a):", typeof(a), "\n")
-    cat("inherits leaflet:", inherits(a, "leaflet"), "\n")
+    cat("inherits leaflet:", inherits(a, "leaflet"), "\n")   
     
-    else {
+    else{
       
       a <- leaflet::addCircleMarkers(
         map = a,
@@ -323,7 +323,7 @@ for(n in seq_along(uni)) {
           opacity = 1
         )
       
-    } else {
+    }else{
       
       a <- leaflet::addCircleMarkers(
         map = a,
